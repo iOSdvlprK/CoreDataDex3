0e8bc9979771314548776ec1f10e5055966d5256
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-09-15 23:57:30 +0900

    widget kit extension

---
+++ CoreDataDex3Widget/CoreDataDex3WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  CoreDataDex3WidgetBundle.swift
+//  CoreDataDex3Widget
+//
+//  Created by joe on 9/15/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CoreDataDex3WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CoreDataDex3Widget()
+    }
+}
